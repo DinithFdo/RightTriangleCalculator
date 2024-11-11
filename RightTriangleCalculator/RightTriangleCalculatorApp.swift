c8b4aa7922c0efe28df61946ee524f3c6bddd7c9
Dinith Fernando <dinithofficial01@gmail.com>
2024-11-11 16:36:46 +0530

    Initial Commit

---
+++ RightTriangleCalculator/RightTriangleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightTriangleCalculatorApp.swift
+//  RightTriangleCalculator
+//
+//  Created by Dinith Fernando on 2024-11-11.
+//
+
+import SwiftUI
+
+@main
+struct RightTriangleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
